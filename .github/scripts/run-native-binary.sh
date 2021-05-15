16e5f2da8910e529b46fd199a34bb88be8e34801
Miguel Ferreira <miguelferreira@me.com>
2021-05-15 10:39:24 +0200

    Test native binary with and without -r option

--- .github/scripts/run-native-binary.sh
+++ .github/scripts/run-native-binary.sh
@@ -4,5 +4,9 @@ IFS=$'\n\t'
 
 build/native-image/application -V
 build/native-image/application -h
+
 build/native-image/application gitlab-clone-example -x tool-test
+[[ ! -f  tool-test/gitlab-clone-example/a-project/some-project-sub-module/README.md ]]
+
+build/native-image/application gitlab-clone-example -x -r tool-test
 [[ -f  tool-test/gitlab-clone-example/a-project/some-project-sub-module/README.md ]]
